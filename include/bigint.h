6ed36db6962267bf8e433ee912bc8ec26de618cd
JustRubik <danghuyhieu1434@gmail.com>
2026-05-18 00:45:52 +0700

    ready for coding

--- include/bigint.h
+++ include/bigint.h
@@ -3,10 +3,10 @@
 #include <stdint.h>
 
 typedef struct {
-  uint32_t *digits; //  chọn base 32
+  uint32_t *digits; //  mảng động uint32, chọn base 32
   size_t size;      //  kích thước tối đa của số
   size_t capacity;  //  Đếu biết, chatGPT gợi ý cái này, vọc sau vậy
 } BigInt;
 
-BigInt normalize(BigInt &num);
+BigInt normalize(BigInt &num);  //  Chuẩn hoá số, xoá chữ số 0 thừa
 
